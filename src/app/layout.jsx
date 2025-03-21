88e5dc1b3890819d4e5a87b911b79b62b1a73803
Ahmed Murtaza <murtazakhan2595@gmail.com>
2025-03-21 22:57:22 +0500

    Added client scripts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,10 @@
 'use client'
 
-import React from 'react'
+import ChatWidget from '@/components/ui/ChatWidget'
 import '@/styles/tailwind.css'
 import Script from 'next/script'
-import ChatWidget from '@/components/ui/ChatWidget'
-import { useState } from 'react'
 
 function RootLayout({ children }) {
-
   return (
     <html lang="en">
       <head>
@@ -17,9 +14,34 @@ function RootLayout({ children }) {
         />
         <Script src="https://api.accessibleagents.com/js/form_embed.js" />
       </head>
-      <body className="antialiased bg-background-1">
+      <body className="bg-background-1 antialiased">
         {children}
-        <ChatWidget/>
+        <ChatWidget />
+        <Script
+          id="px-grabber"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(doc, tag, id){
+                var js = doc.getElementsByTagName(tag)[0];
+                if (doc.getElementById(id)) {return;}
+                js = doc.createElement(tag); 
+                js.id = id;
+                js.src = "https://my.gomobileleads.com/px.min.js";
+                js.type = "text/javascript";
+                doc.head.appendChild(js);
+                js.onload = function() {pxfired();};
+              }(document, 'script', 'px-grabber'));
+              
+              function pxfired() {
+                PxGrabber.setOptions({
+                  Label: "32810787|" + window.location.href,
+                });
+                PxGrabber.render();
+              }
+            `,
+          }}
+        />
       </body>
     </html>
   )
